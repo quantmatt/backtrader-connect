8427f00971f9ec12db579933826210b3fff3a56e
quantmatt <matthew.bailey@enzymelabs.com.au>
2019-12-03 16:16:12 +1100

    configure for heroku

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ const forceSSL = function() {
 app.use(forceSSL());
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/backtrader-connect'));
+app.use(express.static(__dirname + '/dist/backtrader'));
 
 app.get('/*', function(req,res) {    
-	res.sendFile(path.join(__dirname+'/dist/backtrader-connect/index.html'));
+	res.sendFile(path.join(__dirname+'/dist/backtrader/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
