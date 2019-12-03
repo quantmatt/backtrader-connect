f986a0dc9453abeb28c7f41d86161e79d4018e28
quantmatt <matthew.bailey@enzymelabs.com.au>
2019-12-03 16:37:53 +1100

    configure for heroku

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const path = require('path');
 
 const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/backtrader'));
+
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-
 app.get('/*', function(req,res) {    
 	res.sendFile(path.join(__dirname+'/dist/backtrader/index.html'));
 });
@@ -29,10 +31,6 @@ const forceSSL = function() {
 // to use the forceSSL
 // middleware
 app.use(forceSSL());
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/backtrader'));
-
 
 
 // Start the app by listening on the default Heroku port
